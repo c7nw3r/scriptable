944f4658ccc9583f0432c0dde327b380772a4980
christian <christian.weber@leftshift.one>
2022-02-16 19:01:35 +0100

    setup.py modified

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Interpreters'
+        'Topic :: Software Development :: Interpreters',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
